5c6891beb0f8364286d0a1f5b98a6f5e17009a5d
KmL <kamil.bytner@gmai.com>
2020-12-28 20:04:50 +0100

    Create projects for first Microservice

---
+++ ConveyTests/IncidentReport.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IncidentReport.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ ConveyTests/IncidentReport.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IncidentReport.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
